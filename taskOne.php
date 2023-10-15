3ad8e2b63b186b4bc7d71ac0bdb6961f7c0f884b
rudro54 <rudro54@gmail.com>
2023-10-15 23:23:48 +0600

    taskOneCompleted

--- taskOne.php
+++ taskOne.php
@@ -1,1 +1,44 @@
 <?php
+
+$start = 1;
+$end = 20;
+$step = 2;
+
+
+// using for loop
+
+echo "Using For Loop \n";
+
+function printNumbers($start, $end, $step)
+{
+
+    for ($i = $start + 1; $i <= $end; $i += $step) {
+        echo $i . "\n";
+    }
+}
+
+printNumbers($start, $end, $step);
+
+
+echo "Using While Loop \n";
+
+
+// using while loop 
+
+$i = $start + 1;
+while ($i <= $end) {
+    echo $i . "\n";
+    $i += $step;
+}
+
+
+echo "Using do While Loop \n";
+
+// using do while loop
+
+$i = $start + 1;
+
+do {
+    echo $i . "\n";
+    $i += $step;
+} while ($i <= $end);
